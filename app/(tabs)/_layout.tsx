d6fed029c41f0dc40fbb753d0f1016b6b9007950
deepsmc21 <deepthi.mc@gmail,com>
2026-01-22 14:04:40 +0100

    Added Icons back after reset

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,22 @@
 import { Tabs } from "expo-router";
 import React from "react";
+import {Ionicons} from "@expo/vector-icons";
 
 export default function TabLayout() {
     return (
         <Tabs screenOptions={{ headerShown: true }}>
-            <Tabs.Screen name="index" options={{ title: "Heute" }} />
-            <Tabs.Screen name="tasks" options={{ title: "Tasks" }} />
-            <Tabs.Screen name="settings" options={{ title: "Settings" }} />
+            <Tabs.Screen name="index" options={{ title: "Heute" ,
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="calendar-outline" size={size} color={color} />
+                ), }} />
+            <Tabs.Screen name="tasks" options={{ title: "Tasks",
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="list-outline" size={size} color={color} />
+                ), }} />
+            <Tabs.Screen name="settings" options={{ title: "Settings" ,
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="settings-outline" size={size} color={color} />
+                ),}} />
 
             {/* versteckt */}
             <Tabs.Screen name="create" options={{ href: null, title: "Neuer Task" }} />
